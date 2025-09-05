e1e5873934d711fc59911f708bf2760323d575d2
Kyle <113639257+K-Raymer@users.noreply.github.com>
2025-09-05 12:05:16 -0400

    Update payload.py
    Moved Endian to local constant file

--- custom_components/solaredge_modbus/payload.py
+++ custom_components/solaredge_modbus/payload.py
@@ -43,7 +43,7 @@ from array import array
 # pylint: disable=missing-type-doc
 from struct import pack, unpack
 
-from pymodbus.constants import Endian
+from .constants import Endian
 from pymodbus.exceptions import ParameterException
 from pymodbus.logging import Log
 from .utilities import (
